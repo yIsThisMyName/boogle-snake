552aef506f6514a5f01c53fd0a95eab8309b93da
y <luw686021@gmail.com>
2025-11-27 16:29:08 -0800

    Update pauseGameMod.js

--- pauseGameMod.js
+++ pauseGameMod.js
@@ -15,7 +15,7 @@ window.pauseGameMod.runCodeAfter = function() {
   function keydownHandler(e){
     if(e.code === "KeyQ"){
       window.pauseGame = !window.pauseGame;
-      if(window.pauseGame){
+      /*if(window.pauseGame){
         document.querySelector("body > div.Czus3 > div > div.wjOYOd").style.visibility = "visible";
         document.querySelector("body > div.Czus3 > div > div.wjOYOd").style.opacity = 1;
         document.querySelector("body > div.Czus3 > div > div.wjOYOd > div").style.visibility = "hidden";
@@ -23,8 +23,9 @@ window.pauseGameMod.runCodeAfter = function() {
         setTimeout(()=>{if(!window.pauseGame){document.querySelector("body > div.Czus3 > div > div.wjOYOd > div").style.visibility = "visible";}},500);
         document.querySelector("body > div.Czus3 > div > div.wjOYOd").style.visibility = "hidden";
         document.querySelector("body > div.Czus3 > div > div.wjOYOd").style.opacity = 0;
-      }
+      }*/
     }
   }
   document.addEventListener('keydown', keydownHandler);
+
 }
